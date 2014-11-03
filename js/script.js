20ee8b99edde7a06fb0edcfe5efdac970c640e23
lianjinqu2014 <lianjinqu2014@163.com>
2014-11-03 22:26:29 +0800

    finish contact page

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
   $('#main').fullpage({
     responsive: 1,
     resize: true,
-    verticalCentered: true,
 
     css3: true,
     keyboardScrolling: true,
 
     navigation: true,
-    navigationPosition: 'left',
+    navigationPosition: 'right',
 
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
