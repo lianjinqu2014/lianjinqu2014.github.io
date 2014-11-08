6b8902792eca34b33f98229ca000107ca1235076
lianjinqu2014 <lianjinqu2014@163.com>
2014-11-08 21:10:17 +0800

    change coding style

--- js/script.js
+++ js/script.js
@@ -19,54 +19,52 @@ $(document).ready(function() {
     callback: function(){}
   };
 
-  (function(){
-    var onBridgeReady=function(){
-       WeixinJSBridge.on('menu:share:appmessage', function(argv){
-       WeixinJSBridge.invoke('sendAppMessage',{
-       "appid":dataForWeixin.appId ? dataForWeixin.appId : '',
-       "img_url":dataForWeixin.MsgImg,
-       "img_width":"120",
-       "img_height":"120",
-       "link":dataForWeixin.url,
-       "desc":dataForWeixin.desc,
-       "title":dataForWeixin.title
-       }, function(res){(dataForWeixin.callback)();});
-       });
-      WeixinJSBridge.on('menu:share:timeline', function(argv){
-        (dataForWeixin.callback)();
-        WeixinJSBridge.invoke('shareTimeline',{
-          "img_url":dataForWeixin.TLImg,
-          "img_width":"120",
-          "img_height":"120",
-          "link":dataForWeixin.url,
-          "desc":dataForWeixin.desc,
-          "title":dataForWeixin.title
-        }, function(res){});
-      });
-      WeixinJSBridge.on('menu:share:weibo', function(argv){
-        WeixinJSBridge.invoke('shareWeibo',{
-          "content":dataForWeixin.title,
-          "url":dataForWeixin.url
-        }, function(res){(dataForWeixin.callback)();});
-      });
-      WeixinJSBridge.on('menu:share:facebook', function(argv){
-        (dataForWeixin.callback)();
-        WeixinJSBridge.invoke('shareFB',{
-          "img_url":dataForWeixin.TLImg,
-          "img_width":"120",
-          "img_height":"120",
-          "link":dataForWeixin.url,
-          "desc":dataForWeixin.desc,
-          "title":dataForWeixin.title
-        }, function(res){});
-      });
-    };
+  var onBridgeReady = function() {
+    WeixinJSBridge.on('menu:share:appmessage', function(argv){
+      WeixinJSBridge.invoke('sendAppMessage',{
+        "appid":dataForWeixin.appId ? dataForWeixin.appId : '',
+        "img_url":dataForWeixin.MsgImg,
+        "img_width":"120",
+        "img_height":"120",
+        "link":dataForWeixin.url,
+        "desc":dataForWeixin.desc,
+        "title":dataForWeixin.title
+      }, function(res){(dataForWeixin.callback)();});
+    });
+    WeixinJSBridge.on('menu:share:timeline', function(argv){
+      (dataForWeixin.callback)();
+      WeixinJSBridge.invoke('shareTimeline',{
+        "img_url":dataForWeixin.TLImg,
+        "img_width":"120",
+        "img_height":"120",
+        "link":dataForWeixin.url,
+        "desc":dataForWeixin.desc,
+        "title":dataForWeixin.title
+      }, function(res){});
+    });
+    WeixinJSBridge.on('menu:share:weibo', function(argv){
+      WeixinJSBridge.invoke('shareWeibo',{
+        "content":dataForWeixin.title,
+        "url":dataForWeixin.url
+      }, function(res){(dataForWeixin.callback)();});
+    });
+    WeixinJSBridge.on('menu:share:facebook', function(argv){
+      (dataForWeixin.callback)();
+      WeixinJSBridge.invoke('shareFB',{
+        "img_url":dataForWeixin.TLImg,
+        "img_width":"120",
+        "img_height":"120",
+        "link":dataForWeixin.url,
+        "desc":dataForWeixin.desc,
+        "title":dataForWeixin.title
+      }, function(res){});
+    });
+  };
 
-    if(document.addEventListener){
-      document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-    }else if(document.attachEvent){
-      document.attachEvent('WeixinJSBridgeReady'   , onBridgeReady);
-      document.attachEvent('onWeixinJSBridgeReady' , onBridgeReady);
-    }
-  })();
+  if(document.addEventListener){
+    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+  }else if(document.attachEvent){
+    document.attachEvent('WeixinJSBridgeReady'   , onBridgeReady);
+    document.attachEvent('onWeixinJSBridgeReady' , onBridgeReady);
+  }
 });
