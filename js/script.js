34ca970bc7830310c5843d23423607769a160eed
lianjinqu2014 <lianjinqu2014@163.com>
2014-11-08 23:26:04 +0800

    change description

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     TLImg: url + "/img/logo.png",
     url: url,
     title: "连进去-大学生创业实战",
-    desc: "连进去:快速融资、免费找人、创投活动 www.LianJinqu.com",
+    desc: "快速融资、免费找人、创投活动 www.lianjinqu.com",
     callback: function(){}
   };
 
