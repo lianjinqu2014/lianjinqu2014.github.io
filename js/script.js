044901dda6b401bb6bedda44989c590e6ae2faed
lianjinqu2014 <lianjinqu2014@163.com>
2014-11-08 15:50:38 +0800

    remove css3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $('#main').fullpage({
     anchors: ['index', 'quality', 'contact'],
     keyboardScrolling: true,
-    css3: true,
   });
 
   $('#index-arrow').click(function() {
